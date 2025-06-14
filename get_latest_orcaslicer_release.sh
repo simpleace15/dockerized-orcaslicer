c08ab2bd1e17ddc86a49a9711291df07774c502e
SimpleAce <tyler.cleere@gmail.com>
2025-06-13 18:15:09 -0600

    Update get_latest_orcaslicer_release.sh

--- get_latest_orcaslicer_release.sh
+++ get_latest_orcaslicer_release.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
 TMPDIR="$(mktemp -d)"
+
 curl -SsL https://api.github.com/repos/SoftFever/OrcaSlicer/releases/latest > $TMPDIR/latest.json
+
 url=$(jq -r '.assets[] | select(.browser_download_url|test(".*\\.AppImage"))| .browser_download_url' $TMPDIR/latest.json)
 name=$(jq -r '.assets[] | select(.browser_download_url|test(".*\\.AppImage"))| .name' $TMPDIR/latest.json)
 version=$(jq -r .tag_name $TMPDIR/latest.json)
 
-if [[ "$name" == *Ubuntu* ]]; then
-  curl -fSsL $url > /dev/null
-
-  echo "Done! The Ubuntu AppImage file has been downloaded to the current directory."
+if [ $# -ne 1 ]; then
+  echo "Wrong number of params"
+  exit 1
 else
-  echo "No Ubuntu AppImage found in the latest release. Exiting..."
+  request=$1
 fi
+
+case $request in
+
+  url)
+    echo $url
+    ;;
+
+  name)
+    echo $name
+    ;;
+
+  version)
+    echo $version
+    ;;
+
+  *)
+    echo "Unknown request"
+    ;;
+esac
+
+exit 0
