2c6052c4f5e35c648978f64bc8f8cb8d6b19c74b
SimpleAce <tyler.cleere@gmail.com>
2025-06-14 12:26:57 -0600

    Update get_latest_orcaslicer_release.sh

--- get_latest_orcaslicer_release.sh
+++ get_latest_orcaslicer_release.sh
@@ -5,8 +5,8 @@ TMPDIR="$(mktemp -d)"
 curl -SsL https://api.github.com/repos/SoftFever/OrcaSlicer/releases/latest > $TMPDIR/latest.json
 
 # Get the latest stable that contains a matching AppImage
-url=$(jq -r '.assets[] | select(.browser_download_url|test("Linux.*_AppImage_Ubuntu2404_V*.*.*.AppImage$"))| .browser_download_url' $TMPDIR/latest.json)
-name=$(jq -r '.assets[] | select(.browser_download_url|test("Linux.*_AppImage_Ubuntu2404_V*.*.*.AppImage$"))| .name' $TMPDIR/latest.json)
+url=$(jq -r '.assets[] | select(.browser_download_url|test("Linux.*_AppImage_V*.*.*.AppImage$"))| .browser_download_url' $TMPDIR/latest.json)
+name=$(jq -r '.assets[] | select(.browser_download_url|test("Linux.*_AppImage_V*.*.*.AppImage$"))| .name' $TMPDIR/latest.json)
 version=$(jq -r .tag_name $TMPDIR/latest.json)
 
 if [ $# -ne 1 ]; then
