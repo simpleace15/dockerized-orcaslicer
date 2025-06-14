217a8b0bc5dc8fed0ec73e7224ce732a4c51027e
SimpleAce <tyler.cleere@gmail.com>
2025-06-14 14:44:38 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
+
+# Clean X locks
 rm -f /tmp/.X*-lock
 rm -f /tmp/.X11-unix/X*
+
+# Set defaults
 export DISPLAY=${DISPLAY:-:0}
 DISPLAY_NUMBER=$(echo $DISPLAY | cut -d: -f2)
 export NOVNC_PORT=${NOVNC_PORT:-8080}
 export VNC_PORT=${VNC_PORT:-5900}
 export VNC_RESOLUTION=${VNC_RESOLUTION:-1280x800}
+
+# Set VNC password if given
 if [ -n "$VNC_PASSWORD" ]; then
   mkdir -p /root/.vnc
   echo "$VNC_PASSWORD" | vncpasswd -f > /root/.vnc/passwd
@@ -15,7 +21,11 @@ if [ -n "$VNC_PASSWORD" ]; then
 else
   export VNC_SEC="-securitytypes TLSNone,X509None,None"
 fi
+
+# Calculate local framebuffer port
 export LOCALFBPORT=$((${VNC_PORT} + DISPLAY_NUMBER))
+
+# Enable GPU acceleration
 if [ -n "$ENABLEHWGPU" ] && [ "$ENABLEHWGPU" = "true" ]; then
   export VGLRUN="/usr/bin/vglrun"
 else 
@@ -25,5 +35,12 @@ fi
 export SUPD_LOGLEVEL="${SUPD_LOGLEVEL:-TRACE}"
 export VGL_DISPLAY="${VGL_DISPLAY:-egl}"
 
-# fix perms and launch supervisor with the above environment variables
-chown -R orcaslicer:orcaslicer /home/orcaslicer/ /configs/ /prints/ /dev/stdout && exec gosu orcaslicer supervisord -e $SUPD_LOGLEVEL
+# --- NEW: Ensure config dir exists ---
+mkdir -p /configs/.config/OrcaSlicer
+chown -R orcaslicer:orcaslicer /configs/.config
+
+# Fix all other perms
+chown -R orcaslicer:orcaslicer /home/orcaslicer/ /configs/ /prints/ /dev/stdout
+
+# Run supervisord as orcaslicer
+exec gosu orcaslicer supervisord -e "$SUPD_LOGLEVEL"
