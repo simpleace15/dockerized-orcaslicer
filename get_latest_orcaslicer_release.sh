f7e399225319a6c46d53561f20745e6d1a3d0b4c
SimpleAce <tyler.cleere@gmail.com>
2025-06-13 21:53:29 -0600

    Update get_latest_orcaslicer_release.sh

--- get_latest_orcaslicer_release.sh
+++ get_latest_orcaslicer_release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 TMPDIR="$(mktemp -d)"
-
+#curl for latest releases
 curl -SsL https://api.github.com/repos/SoftFever/OrcaSlicer/releases/latest > $TMPDIR/latest.json
 
+# Get the latest stable that contains a matching AppImage
 url=$(jq -r '.assets[] | select(.browser_download_url|test("Linux.*_AppImage_Ubuntu2404_V.*AppImage$"))| .browser_download_url' $TMPDIR/latest.json)
 name=$(jq -r '.assets[] | select(.browser_download_url|test("Linux.*_AppImage_Ubuntu2404_V.*AppImage$"))| .name' $TMPDIR/latest.json)
 version=$(jq -r .tag_name $TMPDIR/latest.json)
@@ -33,5 +34,7 @@ case $request in
     echo "Unknown request"
     ;;
 esac
+
+rm -rf $TMPDIR
 
 exit 0
