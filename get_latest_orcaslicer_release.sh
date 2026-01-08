339d7a306425404d11cb62e546a04f003a2af0b8
simpleace15 <tyler.cleere@gmail.com>
2026-01-07 21:52:53 -0700

    updating url pull

--- get_latest_orcaslicer_release.sh
+++ get_latest_orcaslicer_release.sh
@@ -6,8 +6,8 @@ TMPDIR="$(mktemp -d)"
 curl -SsL https://api.github.com/repos/SoftFever/OrcaSlicer/releases/latest > "$TMPDIR/latest.json"
 
 # Extract fields using jq with more flexible regex to match AppImage
-url=$(jq -r '.assets[] | select(.browser_download_url | test("OrcaSlicer_Linux_AppImage_V[0-9]+\\.[0-9]+\\.[0-9]+(-[0-9a-zA-Z]+)?\\.AppImage$")) | .browser_download_url' "$TMPDIR/latest.json")
-name=$(jq -r '.assets[] | select(.browser_download_url | test("OrcaSlicer_Linux_AppImage_V[0-9]+\\.[0-9]+\\.[0-9]+(-[0-9a-zA-Z]+)?\\.AppImage$")) | .name' "$TMPDIR/latest.json")
+url=$(jq -r '.assets[] | select(.browser_download_url | test("OrcaSlicer_Linux_AppImage_.+?_V[0-9]+\.[0-9]+\.[0-9]+(-[0-9a-zA-Z]+)?\.AppImage$")) | .browser_download_url' "$TMPDIR/latest.json")
+name=$(jq -r '.assets[] | select(.browser_download_url | test("OrcaSlicer_Linux_AppImage_.+?_V[0-9]+\.[0-9]+\.[0-9]+(-[0-9a-zA-Z]+)?\.AppImage$")) | .name' "$TMPDIR/latest.json")
 version=$(jq -r '.tag_name' "$TMPDIR/latest.json")
 
 # Validate input
